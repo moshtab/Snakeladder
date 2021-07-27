70e94473e204bffef7f537d2605736caec594ece
moshtab <mdmohsin41439@gmail.com>
2021-07-27 23:35:30 +0530

    first commit

---
+++ SnakeLadder.java
@@ -0,0 +1,19 @@
+package SnakeLadder;
+class Game{
+	int position=0;
+}
+
+public class SnakeLadder {
+
+	public static void main(String[] args) {
+		
+		Game player1 = new Game();
+		System.out.println("Player at starting position is :" +player1.position);
+		
+		
+	    
+		
+
+	}
+
+}
