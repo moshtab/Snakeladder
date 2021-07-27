ba50d02f753fd7afede0e4bbc2a3202263ed0f83
moshtab <mdmohsin41439@gmail.com>
2021-07-28 01:08:39 +0530

    while loop player reacher to 100

--- SnakeLadder.java
+++ SnakeLadder.java
@@ -10,10 +10,15 @@ public class SnakeLadder {
 
 	public static void main(String[] args) {
 		Game player1 = new Game();
+		while(player1.position<100) {
 		int dicenumber= dice();
 		int option= condition();
 		int move = dicenumber*option;
 		player1.position=player1.position+move;
+		if(player1.position<0) {
+			player1.position=0;
+		}
+		}
 		System.out.println("position of player1 is : " +player1.position);
 		
 		
@@ -33,15 +38,15 @@ public class SnakeLadder {
 		if(flag==0)
 		{
 			temp=0;
-			System.out.println("no play");
+			
 		}
 		else if(flag==1) {
 			temp=1;
-			System.out.println("ladder");
+			
 		}
 		else {
 			temp=-1;
-		   System.out.println("snake");
+		   
 		}
 		
 		return temp;
