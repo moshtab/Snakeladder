37087262f590c080abf6134c8020aa8bfd997827
moshtab <mdmohsin41439@gmail.com>
2021-07-28 00:55:06 +0530

    giving conditions ladde,snake,no play

--- SnakeLadder.java
+++ SnakeLadder.java
@@ -3,20 +3,47 @@ package SnakeLadder;
 import java.util.Random;
 
 class Game{
-	
-	public static int dice() {
-		Random num = new Random();
-		int dicenum = num.nextInt(6)+1;
-		return dicenum;
-	}	
+	int position =0;
 }
 
 public class SnakeLadder {
 
 	public static void main(String[] args) {
 		Game player1 = new Game();
-		System.out.println(player1.dice());
+		int dicenumber= dice();
+		int option= condition();
+		int move = dicenumber*option;
+		player1.position=player1.position+move;
+		System.out.println("position of player1 is : " +player1.position);
+		
+		
 		
 	}
+	private static int dice() {
+		Random r = new Random();
+		int num = r.nextInt(6)+1;
 		
+		return num;
+	}
+
+	private static int condition() {
+		Random r =new Random();
+		int flag = r.nextInt(3);
+		int temp;
+		if(flag==0)
+		{
+			temp=0;
+			System.out.println("no play");
+		}
+		else if(flag==1) {
+			temp=1;
+			System.out.println("ladder");
+		}
+		else {
+			temp=-1;
+		   System.out.println("snake");
+		}
+		
+		return temp;
+	}
 }
